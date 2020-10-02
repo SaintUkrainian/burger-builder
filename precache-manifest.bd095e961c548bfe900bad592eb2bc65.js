30dcd52bd2401116f88d5b3c1209da83d327d67e
Denys Matsenko <idanchik47@gmail.com>
2020-10-02 11:03:48 +0300

    Updates

---
+++ precache-manifest.bd095e961c548bfe900bad592eb2bc65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ba592186353c48477e6736fddbf3e0",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "99df87394964d1748548",
+    "url": "/burger-builder/static/css/main.2cc0708f.chunk.css"
+  },
+  {
+    "revision": "1307d97c9afa43ee6d76",
+    "url": "/burger-builder/static/js/2.4fb6e9dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-builder/static/js/2.4fb6e9dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99df87394964d1748548",
+    "url": "/burger-builder/static/js/main.11f42f91.chunk.js"
+  },
+  {
+    "revision": "3743a9caf0853cc6f96a",
+    "url": "/burger-builder/static/js/runtime-main.f3cd13ce.js"
+  }
+]);
