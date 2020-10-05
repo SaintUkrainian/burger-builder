b601b7c1206b0d958d10e202249d58259344008c
Denys Matsenko <idanchik47@gmail.com>
2020-10-05 23:18:07 +0300

    small optimization update

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,36 @@
 import React from "react";
+import { Component } from "react";
 
 import Aux from "../../../hoc/Auxilary/Auxilary";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-    const ingredientSummary = props.ingredients;
-    const listItems = Object.keys(ingredientSummary).map((key) => (
-        <li key={key}>
-            <span style={{ textTransform: "capitalize" }}>{key}</span>:{" "}
-            {ingredientSummary[key]}x
-        </li>
-    ));
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>Delicious Burger with following ingredients:</p>
-            <ul>{listItems}</ul>
-            <h3>Total price: ${props.totalPrice.toFixed(2)}</h3>
-            <p>Checking out?</p>
-            <Button action={props.cancel} name="Cancel" type="Danger" />
-            <Button action={props.checkOut} name="Checkout!" type="Success" />
-        </Aux>
-    );
+class OrderSummary extends Component {
+
+    componentDidUpdate() {
+        console.log("OrderSummary Did update");
+    }
+
+    render () {
+        const ingredientSummary = this.props.ingredients;
+        const listItems = Object.keys(ingredientSummary).map((key) => (
+            <li key={key}>
+                <span style={{ textTransform: "capitalize" }}>{key}</span>:{" "}
+                {ingredientSummary[key]}x
+            </li>
+        ));
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>Delicious Burger with following ingredients:</p>
+                <ul>{listItems}</ul>
+                <h3>Total price: ${this.props.totalPrice.toFixed(2)}</h3>
+                <p>Checking out?</p>
+                <Button action={this.props.cancel} name="Cancel" type="Danger" />
+                <Button action={this.props.checkOut} name="Checkout!" type="Success" />
+            </Aux>
+        );
+    }
+    
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 
 import styles from "./Modal.module.css";
 
-const modal = (props) => {
-    return (
-        <div
-            className={styles.Modal}
-            style={{
-                transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-                opacity: props.show ? "1" : "0",
-            }}
-        >
-            {props.children}
-        </div>
-    );
-};
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    componentDidUpdate() {
+        console.log("Modal Did update");
+    }
+
+    render() {
+        return (
+            <div
+                className={styles.Modal}
+                style={{
+                    transform: this.props.show
+                        ? "translateY(0)"
+                        : "translateY(-100vh)",
+                    opacity: this.props.show ? "1" : "0",
+                }}
+            >
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export default Modal;
